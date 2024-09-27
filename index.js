268213f579d0798dd2f7c8c8f76067a298cd4061
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-09-27 11:57:27 +0530

    base: index.js

--- index.js
+++ index.js
@@ -3,3 +3,14 @@ import express from "express";
 const app = express();
 
 const port = 3000;
+
+app.get("/api/hello", (req, res) => {
+  res.json({
+    message: "Hello, World!",
+    timestamp: new Date().toISOString(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
