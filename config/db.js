d1663694935ed7d82f4d10d87627252edb476b68
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-09-28 16:03:44 +0530

    fix: adding some comments

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 import mysql from "mysql2";
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "node_cruds",
+  //? Replace these values with your MySQL server credentials
+  host: "localhost", //! Host
+  user: "root", //! Username
+  password: "", //! DB Password
+  database: "", //! DB Name
 });
 
 export default connection;
