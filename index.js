269e08e45228b874dfa8bf9271036cd44d1c6cb1
Bhavin Pathak <dev.pathakbhavin@gmail.com>
2024-10-18 12:57:18 +0530

    fix: env()

--- index.js
+++ index.js
@@ -1,16 +1,11 @@
 import express from "express"; // Import the Express.js framework
 import db from "./config/db.js"; // Import the database configuration
-import env from "dotenv"; // Import dotenv to load environment variables
 import router from "./routes/user_routes.js"; // Import route handler for user-related routes
-
-// Load environment variables from the .env file
-env.config();
-
 // Initialize Express app
 const app = express();
 
 // Define the port on which the server will run, default to 3000 if not specified in .env
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 // Middleware to parse incoming JSON requests
 app.use(express.json());
